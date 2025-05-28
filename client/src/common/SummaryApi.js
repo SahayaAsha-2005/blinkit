bc763ced4184c3f64a35e8d4554b9a16afa5a683
Sahaya Asha <144304039+SahayaAsha-2005@users.noreply.github.com>
2025-05-28 12:29:51 +0530

    Update SummaryApi.js

--- client/src/common/SummaryApi.js
+++ client/src/common/SummaryApi.js
@@ -1,4 +1,5 @@
-export const baseURL = "http://localhost:8080"
+export const baseURL = "https://blinkit-1nlz.onrender.com"
+// export const baseURL = "http://localhost:8080"
 
 const SummaryApi = {
     register : {
